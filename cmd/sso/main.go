29a13afbd87931cade0ecb878b182c07ecb2eafa
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2025-01-06 20:17:01 +0400

    defer graceful shutdown with chan + os.Signal

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,59 +1,75 @@
 package main
 
 import (
-  "log/slog"
-  "os"
-  "sso/internal/app"
-  "sso/internal/config"
-  "sso/internal/lib/logger/handlers/slogpretty"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"sso/internal/app"
+	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+	"syscall"
 )
 
 const (
-  envLocal = "local"
-  envDev   = "dev"
-  envProd  = "prod"
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-  cfg := config.MustLoad()
+	cfg := config.MustLoad()
 
-  log := setupLogger(cfg.Env)
+	log := setupLogger(cfg.Env)
 
-  log.Info("starting application", slog.Any("cfg", cfg))
+	log.Info("starting application", slog.Any("cfg", cfg))
 
-  application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
 
-  application.GRPCSrv.MustRun()
+	go application.GRPCSrv.MustRun()
 
-  // TODO: launch gRPC-server apps
+	defer listenToGracefulShutdown(log, application)
 
+	// TODO: launch gRPC-server apps
+
+	fmt.Println("🟢 main bottom")
 }
 
 func setupLogger(env string) *slog.Logger {
-  var log *slog.Logger
+	var log *slog.Logger
 
-  switch env {
-  case envLocal:
-    log = setupPrettySlog()
-  case envDev:
-    log = slog.New(
-      slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-    )
-  case envProd:
-    log = slog.New(
-      slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-  }
-  return log
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
 
 func setupPrettySlog() *slog.Logger {
-  opts := slogpretty.PrettyHandlerOptions{
-    SlogOpts: &slog.HandlerOptions{
-      Level: slog.LevelDebug,
-    },
-  }
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
 
-  handler := opts.NewPrettyHandler(os.Stdout)
+	handler := opts.NewPrettyHandler(os.Stdout)
 
-  return slog.New(handler)
+	return slog.New(handler)
+}
+
+func listenToGracefulShutdown(log *slog.Logger, application *app.App) {
+	fmt.Println("🟢 listenToGracefulShutdown")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	sign := <-stop
+	log.Info("stopping application", slog.String("signal", sign.String()))
+	application.GRPCSrv.Stop()
+	log.Info("application stopped")
 }
